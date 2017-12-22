02d68e8ecc3575f24af71e768c62709c4c2265f2
ocean <ocean@DESKTOP-793P4V7>
2017-12-22 09:50:06 +0800

    init

---
+++ src/bhz/netty/udp/ClientHandler.java
@@ -0,0 +1,32 @@
+package bhz.netty.udp;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.channel.socket.DatagramPacket;
+import io.netty.util.CharsetUtil;
+
+
+public class ClientHandler extends SimpleChannelInboundHandler<DatagramPacket> {
+
+    public void messageReceived(ChannelHandlerContext ctx, DatagramPacket msg)
+	    throws Exception {
+		String response = msg.content().toString(CharsetUtil.UTF_8);
+		if (response.startsWith("谚语查询结果: ")) {
+		    System.out.println(response);
+		    ctx.close();
+		}
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause)
+	    throws Exception {
+		cause.printStackTrace();
+		ctx.close();
+    }
+
+	@Override
+	protected void channelRead0(ChannelHandlerContext arg0, DatagramPacket arg1) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+}
